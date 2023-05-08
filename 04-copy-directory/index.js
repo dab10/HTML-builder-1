038ba9ca67f797ae99084e684dabab124f14f31b
dab10 <dab10@list.ru>
2023-05-08 20:48:13 +0300

    feat: finish the fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.readdir(path.join(__dirname, 'files'), 
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, err => {
+      if (err) throw err;
+
+      fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+          if (err) throw err;
+        });
+      });
+
+    });
+        
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+        if (err) throw err;
+      });
+    });
+    
+  }
+);
